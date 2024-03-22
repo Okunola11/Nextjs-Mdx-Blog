bf3fdb9e809233fb6a3abf233f26fca5ffa79cac
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-03-22 11:13:49 +0100

    Rewrote the data fetching method for posts

--- lib/post.tsx
+++ lib/post.tsx
@@ -1,4 +1,87 @@
-import fs from "fs";
+import { compileMDX } from "next-mdx-remote/rsc";
+import { title } from "process";
+
+type Filetree = {
+  "tree": [
+    {
+      "path": string;
+    }
+  ];
+};
+
+export async function getPostByName(
+  fileName: string
+): Promise<BlogPost | undefined> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/okunola11/blogPosts-test/main/${fileName}`,
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+  if (!res.ok) return undefined;
+
+  const rawMDX = await res.json();
+
+  if (rawMDX === "404: Not Found") return undefined;
+
+  const { frontmatter, content } = await compileMDX<{
+    title: string;
+    date: string;
+    tags: string[];
+  }>({ source: rawMDX });
+
+  const id = fileName.replace(/\.mdx$/, "");
+
+  const blogPostObj: BlogPost = {
+    meta: {
+      id,
+      title: frontmatter.title,
+      date: frontmatter.date,
+      tags: frontmatter.tags,
+    },
+    content,
+  };
+}
+
+export async function getPostsMeta(): Promise<Meta[] | undefined> {
+  const res = await fetch(
+    "https://api.github.com/repos/okunola11/blogPosts-test/git/trees/main?recursive=1",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+  if (!res.ok) return undefined;
+
+  const repoFiletree: Filetree = await res.json();
+
+  const filesArray = repoFiletree.tree
+    .map((obj) => obj.path)
+    .filter((path) => path.endsWith(".mdx"));
+  // there are images and other files in the blogPosts repo but we just want the mdx files
+  // mapping then filtering makes sure to return an array not an Object. That's what just filtering would do
+
+  const posts: Meta[] = [];
+
+  for (const file of filesArray) {
+    const post = await getPostByName(file);
+    if (post) {
+      const { meta } = post;
+      posts.push(meta);
+    }
+  }
+
+  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+/* import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { remark } from "remark";
@@ -56,3 +139,4 @@ export async function getPostData(id: string) {
   // Combine the data with the id
   return blogPostWithHTML;
 }
+ */
