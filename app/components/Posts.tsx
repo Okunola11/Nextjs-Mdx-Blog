22f3f49e8a23397dea9ca43ee8aa7d1199deab16
Abdulwasiu <abdayoade@gmail.com>
2024-04-07 23:53:46 +0100

    Added Hero section

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -2,6 +2,7 @@ import { getPostsMeta } from "@/lib/post";
 import PostsList from "./PostsList";
 import Link from "next/link";
 import TagList from "./TagList";
+import SparklesPreview from "./Hero";
 
 export default async function Posts() {
   const posts = await getPostsMeta(); // we dont need to async/await, the data is on localStorage
@@ -29,7 +30,9 @@ export default async function Posts() {
         </div>
       </section>
  */}
-      <section className="h-screen bg-white"></section>
+      <section className="">
+        <SparklesPreview />
+      </section>
 
       <section className="grid grid-cols-1 md:grid-cols-3 relative p-0 m-0 w-full">
         <ul className="md:col-span-2 order-2 md:order-1 md:pt-16 p-0 md:pl-6 md:max-w-2xl flex flex-col">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -22,5 +26,18 @@ module.exports = {
       };
       addUtilities(newUtillities, ["responsive", "hover"]);
     },
+    addVariablesForColors,
   ],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
