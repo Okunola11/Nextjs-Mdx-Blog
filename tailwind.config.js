90720f83d270de0fdfdea80898ec713d3d254236
Abdulwasiu <abdayoade@gmail.com>
2024-04-09 12:50:03 +0100

    removed the added themes, realized they were not needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,30 +11,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    // themes from "shadcn"
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+    extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
@@ -51,7 +28,6 @@ module.exports = {
       addUtilities(newUtillities, ["responsive", "hover"]);
     },
     addVariablesForColors, // from "aceternity"
-    require("tailwindcss-animate"), // from "shadcn ui"
   ],
 };
 
