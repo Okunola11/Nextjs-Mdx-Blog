c9c08b5fd33fa5fc2456c66e5dd1a4bfec95935a
Abdulwasiu <abdayoade@gmail.com>
2024-04-10 06:52:07 +0100

    Corrected name spelling @about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,15 +4,17 @@ export const revalidate = 0;
 
 export default function About() {
   return (
-    <div className="px-4 md:px-6 max-w-[800px] mx-auto">
+    <div className="h-screen px-4 md:px-6 max-w-[800px] mx-auto">
       <MyProfilePic />
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello, welcome 👋&nbsp;to my blog!
         <br />
         It's a pleasure to have you here.{" "}
-        <span className="whitespace-nowrap font-bold">I'm Waasi</span> and I'm
-        excited to embark on this journey of sharing insights, experiences, and
-        knowledge with you.
+        <span className="whitespace-nowrap font-bold">
+          I'm Okunola Abdulwasiu
+        </span>{" "}
+        and I'm excited to embark on this journey of sharing insights,
+        experiences, and knowledge with you.
       </p>
     </div>
   );
